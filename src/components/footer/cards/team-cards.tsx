48c230510cf7f89396d1bb1d11707fd17e3661ac
RomanikhinMikhail <mihail19092005@gmail.com>
2024-12-20 18:00:19 +0300

    change styles

--- src/components/footer/cards/team-cards.tsx
+++ src/components/footer/cards/team-cards.tsx
@@ -6,23 +6,24 @@ import im3 from '../../../assets/cards/Romanikhin.jpg';
 import im4 from '../../../assets/cards/Uvaisov.jpg';
 
 const TeamCards = () => {
-    const team: Array<Record<string, string>> = [
-        { 'name': 'Медведев Евгений', 'src' : im1},
-        { 'name': 'Павлюченко Михаил', 'src' : im2},
-        { 'name': 'Романихин Михаил', 'src' : im3},
-        { 'name': 'Увайсов Магомедрасул', 'src' : im4},
-    ]
 
-    return(
-        <div className={styles.wrapper}>
-            <h2>Наша команда:</h2><br />
-            <div className={styles.cardsdisplay}>
-                {team.map(member => (
-                    <Card key={member.name} name={member.name} src={member.src} width='100%' height='auto'/> //imgsrc='./Medvedev.png'/>
-                ))}
-            </div>
-        </div>
-    )
+	const team: Array<Record<string, string>> = [
+		{ 'name': 'Медведев Евгений', 'src' : im1},
+		{ 'name': 'Павлюченко Михаил', 'src' : im2},
+		{ 'name': 'Романихин Михаил', 'src' : im3},
+		{ 'name': 'Увайсов Магомедрасул', 'src' : im4},
+	]
+
+	return(
+		<div className={styles.wrapper}>
+			<h2>Наша команда:</h2>
+			<div className={styles.cardsdisplay}>
+				{team.map(member => (
+						<Card key={member.name} name={member.name} src={member.src} width='100%' height='auto'/> //imgsrc='./Medvedev.png'/>
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default TeamCards;
